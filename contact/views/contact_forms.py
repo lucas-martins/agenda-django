4d01d34da37a4e5ca80b0eba3c383b904a5b2983
Lucas Martins <lucasf.martins94@outlook.com>
2025-03-24 14:57:42 -0300

    Utilizando request.method e request.POST

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,6 +4,10 @@ from django.core.paginator import Paginator
 from contact.models import Contact
 
 def create(request):
+    if request.method == 'POST':
+        print(request.method)
+        print(request.POST.get('first_name'))
+
     context = {
 
     }
