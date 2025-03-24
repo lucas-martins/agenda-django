965ef888499c4ee0cd8233af66c5041536258e6d
Lucas Martins <lucasf.martins94@outlook.com>
2025-03-24 14:12:08 -0300

    Inclusão de paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
 from contact.models import Contact
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True) \
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -35,9 +40,13 @@ def search(request):
             Q(email__icontains=search_value)
         ) \
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
