09ab6106e513b7292a41a76b9c93fb468dfe54b8
Lucas Martins <lucasf.martins94@outlook.com>
2025-03-25 14:30:06 -0300

    Adição de mensagens de erro

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django import forms
+from django.core.exceptions import ValidationError
 
 from contact.models import Contact
 
@@ -9,6 +10,19 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'phone',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError('Mensagem de erro', code='invalid')
+        )
+        self.add_error(
+            None,
+            ValidationError('Mensagem de erro', code='invalid')
+        )
+        return super().clean()
 
 def create(request):
     if request.method == 'POST':
