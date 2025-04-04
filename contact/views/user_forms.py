f2092b27fa9abb10bd6807cba2326c0429d55e58
Lucas Martins <lucasf.martins94@outlook.com>
2025-04-04 19:20:49 -0300

    Garantindo que usuário está logado para acessar a view

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 from contact.forms import RegisterForm, RegisterUpdateForm
 
@@ -44,10 +45,12 @@ def login_view(request):
         }
     )
 
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
 
@@ -72,4 +75,4 @@ def user_update(request):
         )
     
     form.save()
-    return redirect('contact:login')
+    return redirect('contact:user_update')
